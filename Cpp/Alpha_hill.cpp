b4b1f30a11a93be7b83973b1cc6f443cd48c21c1
Gopi-githubb <138892791+Gopi-githubb@users.noreply.github.com>
2024-09-16 23:43:15 +0530

    Cpp

---
+++ Cpp/Alpha_hill.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+void alphaHill(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+
+        char ch = 'A';
+        int breakpoint = (2 * i + 1) / 2;
+        for (int j = 0; j <= 2 * i; j++)
+        {
+            cout << ch << " ";
+            if (j < breakpoint)
+            {
+                ch++;
+            }
+            else
+            {
+                ch--;
+            }
+        }
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    alphaHill(n);
+    return 0;
+}
